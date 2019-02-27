092bd98153d7a6aafd76ab8da28ce73b9c42dab4
AaronLenoir <lenoir.aaron@gmail.com>
2019-02-28 00:16:56 +0100

    Add knockout JS

---
+++ src/Kicker.Stats/wwwroot/js/app.js
@@ -0,0 +1,12 @@
+﻿let KickerStatsAppViewModel = function () {
+
+    let self = this;
+
+    self.appName = "Kicker Stats";
+
+    self.loading = ko.observable(true);
+
+    self.loadData = function (data) {
+        self.loading(false);
+    };
+};
