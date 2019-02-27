7166c0dd0bd522354f213ef0178676ec9b068f48
AaronLenoir <lenoir.aaron@gmail.com>
2019-02-27 23:57:03 +0100

    Add ASP.NET Core with JavaScript Data fetch

---
+++ src/Kicker.Stats/wwwroot/js/statsDataService.js
@@ -0,0 +1,16 @@
+﻿let kickerStatsDataService = (function () {
+    let fetchData = function (callback) {
+        fetch('api/values')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(JSON.stringify(data));
+                callback(data);
+            });
+    };
+
+    return {
+        fetchData: fetchData
+    };
+}());
