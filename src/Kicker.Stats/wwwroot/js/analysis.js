89b2761e8bafa2fd4b9817033a0be6f95cb5d9d8
AaronLenoir <lenoir.aaron@gmail.com>
2019-02-28 01:17:59 +0100

    Add TeamStats

--- src/Kicker.Stats/wwwroot/js/analysis.js
+++ src/Kicker.Stats/wwwroot/js/analysis.js
@@ -1,6 +1,92 @@
-﻿let KickerStatsAnalysis = function (data) {
+﻿let KickerStatsAnalysis = (function (data) {
+    let Team = function (keeper, striker) {
+        let self = this;
 
-    let self = this;
+        self.keeper = keeper;
+        self.striker = striker;
+        self.getTeamId = function () {
+            return keeper + " - " + striker;
+        };
+    };
 
-    self.rawData = data;
-}
+    let TeamStats = function (team) {
+        let self = this;
+
+        self.team = team;
+        self.gamesWon = 0;
+        self.gamesPlayed = 0;
+
+        self.addResult = function(ourScore, otherScore) {
+            if (ourScore > otherScore) {
+                self.gamesWon++;
+            }
+
+            self.gamesPlayed++;
+        }
+    };
+
+    let loadTeamStats = function (rawData, allPlayers) {
+        let result = [];
+
+        // Create all possible teams (even if no games are played that way)
+        let teams = [];
+
+        for (let i = 0; i < allPlayers.length; i++) {
+            for (let j = 0; j < allPlayers.length; j++) {
+                let keeper = allPlayers[i];
+                let striker = allPlayers[j];
+
+                if (keeper === striker) { continue; }
+
+                let team = new Team(keeper, striker)
+                teams[team.getTeamId()] = new TeamStats(team);
+            }
+        }
+
+        for (let i = 0; i < rawData.length; i++) {
+            let gameResult = rawData[i];
+
+            let teamA = new Team(gameResult.keeperA, gameResult.strikerA);
+            let teamB = new Team(gameResult.keeperB, gameResult.strikerB);
+
+            teams[teamA.getTeamId()].addResult(gameResult.scoreA, gameResult.scoreB);
+            teams[teamB.getTeamId()].addResult(gameResult.scoreB, gameResult.scoreA);
+        }
+
+        for (let key in teams) {
+            if (teams[key].gamesPlayed === 0) { continue; }
+
+            result.push(teams[key]);
+        }
+
+        return result;
+    };
+
+    let getAllPlayers = function (rawData) {
+        let allPlayers = [];
+
+        for (let i = 0; i < rawData.length; i++) {
+            let playerTypes = ["keeperA", "strikerA", "keeperB", "strikerB"];
+
+            let gameResult = rawData[i];
+
+            for (let j = 0; j < playerTypes.length; j++) {
+                let playerType = playerTypes[j];
+
+                let player = gameResult[playerType];
+
+                if (allPlayers.indexOf(player) === -1) { allPlayers.push(player); }
+            }
+        }
+
+        return allPlayers;
+    };
+
+    return function (data) {
+        let self = this;
+
+        self.rawData = data;
+        self.allPlayers = getAllPlayers(self.rawData);
+        self.teamStats = loadTeamStats(self.rawData, self.allPlayers);
+    }
+}());
