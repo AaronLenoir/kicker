3d89b2aa18739ba0d164d9dca272715c4ea2ffbc
AaronLenoir <lenoir.aaron@gmail.com>
2019-02-28 01:21:51 +0100

    Add some more test data

--- src/Kicker.Stats/Controllers/ResultsController.cs
+++ src/Kicker.Stats/Controllers/ResultsController.cs
@@ -16,7 +16,11 @@ namespace Kicker.Stats.Controllers
             return new List<GameResult>
             {
                 new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 10, 6),
-                new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 6, 10)
+                new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 6, 10),
+                new GameResult(DateTime.Now, "Aaron", "Szilard", "Wim", "Koen", 10, 6),
+                new GameResult(DateTime.Now, "Anthony", "Vincent", "Wim", "Koen", 6, 10),
+                new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 10, 6),
+                new GameResult(DateTime.Now, "Anthony", "Bruno", "Wim", "Vincent", 6, 10)
             };
         }
     }
