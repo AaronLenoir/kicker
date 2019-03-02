ca026d2e37e3d16f4f7737d7e87010e3ae76617c
AaronLenoir <lenoir.aaron@gmail.com>
2019-03-03 00:02:45 +0100

    Remove old code

--- src/Kicker.Stats/Services/GoogleDocsRepository.cs
+++ src/Kicker.Stats/Services/GoogleDocsRepository.cs
@@ -17,15 +17,6 @@ namespace Kicker.Stats.Services
 
         public IEnumerable<GameResult> GetResults()
         {
-            //return new List<GameResult>
-            //{
-            //    new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 10, 6),
-            //    new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 6, 10),
-            //    new GameResult(DateTime.Now, "Aaron", "Szilard", "Wim", "Koen", 10, 6),
-            //    new GameResult(DateTime.Now, "Anthony", "Vincent", "Wim", "Koen", 6, 10),
-            //    new GameResult(DateTime.Now, "Aaron", "Bruno", "Wim", "Koen", 10, 6),
-            //    new GameResult(DateTime.Now, "Anthony", "Bruno", "Wim", "Vincent", 6, 10)
-            //};
             var result = new List<GameResult>();
 
             var credential = GetCredential();
